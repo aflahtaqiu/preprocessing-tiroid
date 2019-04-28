dd855ee00de71bede78b7b808d3b536347284d98
aflahtaqiu <aflahts98@gmail.com>
2019-04-28 13:25:57 +0700

    add minmax normalization

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 import csv
 import numpy as np
 import impyute as imp
+from sklearn.preprocessing import MinMaxScaler
 
 data_arrays = list()
-results = []
 with open('data/data_tiroid_missing.csv', 'r') as csvFile:
     reader = csv.reader(csvFile)
     for row in reader:
         data_arrays.append(row)
 
-for i , itemi in enumerate(data_arrays):
-    for j, itemj in enumerate(data_arrays[i]):
-        if(itemj == '?'):
-            data_arrays[i][j] = np.nan
-        else:   
-            data_arrays[i][j] = float(itemj)
+def setMissingValues(data):
+    for i, itemi in enumerate(data):
+        for j, itemj in enumerate(data[i]):
+            if(itemj == '?'):
+                data[i][j] = np.nan
+            else:
+                data[i][j] = float (itemj)
 
-with open('data/new_tiroid.csv', 'w') as csvFile:
-    writer = csv.writer(csvFile)
-    writer.writerows(imp.fast_knn(np.array(data_arrays)).tolist())
-csvFile.close()
+    with open('data/new_tiroid.csv', 'w') as csvFile :
+        writer = csv.writer(csvFile)
+        data = imp.fast_knn(np.array(data)).tolist()
+        writer.writerows(data)
+    csvFile.close()
+
+    return data
+
+def setMinMaxNormalization(data):
+    minmax_scaler = MinMaxScaler()
+    data_minmax = list()
+    data_minmax = minmax_scaler.fit_transform(data).tolist()
+
+    with open('data/minmax_new_tiroid.csv', 'w') as csvFile :
+        writer = csv.writer(csvFile)
+        writer.writerows(data_minmax)
+    csvFile.close()
+
+new_data = list()
+new_data = setMissingValues(data_arrays)
+setMinMaxNormalization(new_data)
+
