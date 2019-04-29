d6d4b08e139bbcebe9b8b20f49ef791fbff6ee4b
aflahtaqiu <aflahts98@gmail.com>
2019-04-29 20:17:41 +0700

    knn

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ with open('data/data_tiroid_missing.csv', 'r') as csvFile:
     for row in reader:
         data_arrays.append(row)
 
+data_label = np.array(data_arrays)[:, 5].tolist()
+
 def setMissingValues(data):
     for i, itemi in enumerate(data):
         for j, itemj in enumerate(itemi):
@@ -27,31 +29,48 @@ def setMissingValues(data):
         writer.writerows(data)
     csvFile.close()
 
+    data_nonlabel = np.array(data)[:, :5].tolist()
+    print(data_nonlabel)
+    data_length = len(data_nonlabel)
+    knn.fit(data_nonlabel, data_label)
+    prediksi = knn.predict(data_nonlabel)
+    beda = 0
+
+    for i, item in enumerate(data_label):
+        if(item != prediksi[i]):
+            beda = beda +1
+
+    error = (beda/data_length)*100
+    print("Error sebelum normalisasi ",error,"%")
+
     return data
 
 def setMinMaxNormalization(data):
     minmax_scaler = MinMaxScaler()
     data_minmax = data
-    data_label = np.array(data)[:, 5].tolist()
     data_minmax = minmax_scaler.fit_transform(data)[:,:5].tolist()
-    
-    knn.fit(data_arrays, data_label)
 
     for i, itemi in enumerate(data_minmax):
         data_minmax[i].append(data_label[i])
-
-    hasil = knn.predict(data_minmax)
-    print(hasil)
 
     with open('data/minmax_new_tiroid.csv', 'w') as csvFile :
         writer = csv.writer(csvFile)
         writer.writerows(data_minmax)
     csvFile.close()
 
+    data_length = len(data_minmax)
+    knn.fit(data_minmax, data_label)
+    prediksi = knn.predict(data_minmax)
+    beda = 0
+    for i, item in enumerate(data_label):
+        if(item != prediksi[i]):
+            beda = beda +1
+    error = (beda/data_length)*100
+    print("Error normalisasi minmax ",error,"%")
+
 def setZscoreNormalization(data) :
     data_zscore = list()
     data_zscore = data
-    data_label = np.array(data)[:, 5].tolist()
     data_zscore = stats.zscore(data)[:,:5].tolist()
 
     for i, itemi in enumerate(data_zscore):
@@ -61,6 +80,16 @@ def setZscoreNormalization(data) :
         writer = csv.writer(csvFile)
         writer.writerows(data_zscore)
     csvFile.close()
+
+    data_length = len(data_zscore)
+    knn.fit(data_zscore, data_label)
+    prediksi = knn.predict(data_zscore)
+    beda = 0
+    for i, item in enumerate(data_label):
+        if(item != prediksi[i]):
+            beda = beda +1
+    error = (beda/data_length)*100
+    print("Error normalisasi zscore ",error,"%")
 
 def sigmoid(x):
     import math
@@ -80,6 +109,16 @@ def setSigmoidNormalization(data):
         writer = csv.writer(csvFile)
         writer.writerows(data_sigmoid)
     csvFile.close()
+
+    data_length = len(data_sigmoid)
+    knn.fit(data_sigmoid, data_label)
+    prediksi = knn.predict(data_sigmoid)
+    beda = 0
+    for i, item in enumerate(data_label):
+        if(item != prediksi[i]):
+            beda = beda +1
+    error = (beda/data_length)*100
+    print("Error normalisasi sigmoid ",error,"%")
 
 
 new_data = list()
